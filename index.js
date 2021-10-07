7dcc271f22a5495a5cf18b3c50532572d9973d46
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-07 14:13:47 -0400

    Employee added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+
+generateHtml (fileName, data){
+    fs.writeFile(fileName, (data), (err) => {
+        if (err) {
+          return console.log(err);
+        }
+  
+        console.log("Success! Your team is done.");
+      
+    });
+    
+    function init(){
+        inquirer.prompt(questions)
+        .then(function (userInput){
+          console.log(userInput)
+         writeToFile("index.html", generateHtml(userInput));
+        });
+      };
+      
+}
