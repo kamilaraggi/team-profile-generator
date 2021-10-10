f39d145fd8fa500beb5daaeae4463e9a5290f6d0
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-10 13:54:19 -0400

    add team html skeleton

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const fs = require ('fs');
+const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+const Enginner = require('./lib/Enginner');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
 
 generateHtml (fileName, data){
     fs.writeFile(fileName, (data), (err) => {
         if (err) {
           return console.log(err);
+      
         }
   
         console.log("Success! Your team is done.");
