475dbb392c947c40d89dceddb7fa5d0ecc2ba4ba
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-10 17:40:54 -0400

    add team member functions

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const Employee = require('./lib/Employee');
 const Enginner = require('./lib/Enginner');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
-const outPut = path.join("team.html");
+//const path = require("path");
+const roleArr = []
 
-function questions (){
+function questions(){
   inquirer
   .prompt([
     {
@@ -25,7 +26,7 @@ function questions (){
       name:"email",
     },
     {
-      type:"list"
+      type:"list",
       message:"Whats their role?",
       choices: ["Manager", "Enginner", "Intern"],
       name:"role",
@@ -40,23 +41,103 @@ function questions (){
 }
 
 
-generateHtml (fileName, data){
-    fs.writeFile(fileName, (data), (err) => {
-        if (err) {
-          return console.log(err);
-      
-        }
-  
-        console.log("Success! Your team is done.");
-      
-    });
-    
-    function init(){
-        inquirer.prompt(questions)
-        .then(function (userInput){
-          console.log(userInput)
-         writeToFile("index.html", generateHtml(userInput));
-        });
-      };
-      
+function phone(generalAnswers){
+  inquirer
+  .prompt([
+    {
+      type:"input",
+      massage:"Enter their Phone Number:",
+      name:"Phone",
+    },
+    {
+      type:"confirm",
+      message:"All set for this one! Do you wanna add another team member?",
+      name:"add",
+    }
+  ]).then(function (answers){
+    const newManager = new Manager(generalAnswers.name, generalAnswers.id, generalAnswers.email, answers.phone)
+    roleArr.push(newManager);
+    if ( answers.add === true){
+      questions()
+    } else {
+      buildTeam();
+      console.log("Your Team has been completed!")
+    }
+  })
 }
+
+function git(generalAnswers){
+  inquirer
+  .prompt([
+    {
+      type:"input",
+      massage:"Enter their Github username:",
+      name:"Git",
+    },
+    {
+      type:"confirm",
+      message:"All set for this one! Do you wanna add another team member?",
+      name:"add",
+    }
+  ]).then(function (answers){
+    const newEnginner = new Enginner(generalAnswers.name, generalAnswers.id, generalAnswers.email, answers.git)
+    roleArr.push(newEnginner);
+    if ( answers.add === true){
+      questions()
+    } else {
+      buildTeam();
+      console.log("Your Team has been completed!")
+    }
+  })
+}
+
+function school(generalAnswers){
+  inquirer
+  .prompt([
+    {
+      type:"input",
+      massage:"Enter their School:",
+      name:"School",
+    },
+    {
+      type:"confirm",
+      message:"All set for this one! Do you wanna add another team member?",
+      name:"add",
+    }
+  ]).then(function (answers){
+    const newIntern = new Intern (generalAnswers.name, generalAnswers.id, generalAnswers.email, answers.school)
+    roleArr.push(newIntern);
+    if ( answers.add === true){
+      questions()
+    } else {
+      buildTeam();
+      console.log("Your Team has been completed!")
+    }
+  })
+}
+
+//function buildTeam(){
+//fs.writeFileSync(outputPath, render(roleArr),"utf-8");
+//}
+//generateHtml (fileName, data){
+  //  fs.writeFile(fileName, (data), (err) => {
+  //      if (err) {
+  //      return console.log(err);
+  //  
+  //  }
+  //
+  //    console.log("Success! Your team is done.");
+  //
+  // });
+    
+   // function init(){
+     //   inquirer.prompt(questions)
+       // .then(function (userInput){
+         // console.log(userInput)
+         //writeToFile("index.html", generateHtml(userInput));
+       // });
+     // };
+      
+//}
+
+questions();
