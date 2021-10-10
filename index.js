e62a56d052981ba983021c3b17cf2980d38fa7ba
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-10 15:13:29 -0400

    general questions prompt

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const Employee = require('./lib/Employee');
 const Enginner = require('./lib/Enginner');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+const outPut = path.join("team.html");
+
+function questions (){
+  inquirer
+  .prompt([
+    {
+    type: "input",
+    message: "whats your name?",
+    name:"name",
+    },
+    {
+      type:"input",
+      message: "what you employee ID?",
+      name:"id",
+    },
+    {
+      type:"input",
+      message:"Whats your E-mail Address?",
+      name:"email",
+    },
+    {
+      type:"list"
+      message:"Whats their role?",
+      choices: ["Manager", "Enginner", "Intern"],
+      name:"role",
+    },
+  ]).then(function(answers) {
+   if(answers.role === "Manager") {
+     phone(answers);
+   } else if (answers.role === "Enginner") {
+     git(answers);
+   } else {school(answers)};
+  })
+}
+
 
 generateHtml (fileName, data){
     fs.writeFile(fileName, (data), (err) => {
